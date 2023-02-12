22cc3bb2575e6bdda1224a82e64a785f1a510f37
Anton Belski <anton.belski@gmail.com>
2023-02-12 17:51:58 +0300

    feat:add solution

--- src/index.js
+++ src/index.js
@@ -37,8 +37,44 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
+function decode(str) {
+    let inputData = str.split("**********")
+  let words = [];
+  
+  inputData.forEach(item => {
+    let word = [];
+    item = item.split("");
+
+    while (item.length !== 0) {
+      let symbol = item.splice(0,2).join('');
+      if (symbol == "00") {
+        word.push("");
+      } else if (symbol == "10") {
+        word.push(".");
+      } else {
+        word.push("-");
+      }
+    }
+  
+    words.push(word);
+  });
+  
+  let getLetters = () => {
+    let res = [];
+
+    words.forEach((item) => {
+    
+      let word = [];
+      while (item.length !== 0) {
+        word.push(item.splice(0,5))
+      }
+      res.push(word);
+    })
+
+    return res;
+  }
+  let decoded = getLetters().map((e) => e.map((item) => MORSE_TABLE[item.join("")]));
+  return decoded.map((e) => e.join("")).join(" ");
 }
 
 module.exports = {
